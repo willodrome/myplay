aab2cfcfabdd2849075d8e898308c33bd2abd21c
willodrome <65831821+willodrome@users.noreply.github.com>
2021-02-06 20:26:35 +0200

    Added service-workers

--- client/src/index.js
+++ client/src/index.js
@@ -2,8 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 import reportWebVitals from "./reportWebVitals";
-import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
   <React.StrictMode>
@@ -12,5 +12,5 @@ ReactDOM.render(
   document.getElementById("root")
 );
 
+serviceWorkerRegistration.register();
 reportWebVitals();
-serviceWorker.register();
